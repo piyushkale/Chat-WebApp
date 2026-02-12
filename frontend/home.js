41dec1ab0830b3642a163d61323a245bbae99283
Piyush Kale <97799152+piyushkale@users.noreply.github.com>
2026-02-12 10:11:24 +0530

    Chat Interface

--- frontend/home.js
+++ frontend/home.js
@@ -3,9 +3,13 @@ window.onload = () => {
   token = localStorage.getItem("token");
   if (!token) {
     window.location.href = "/index.html";
-    return
+    return;
   }
-  
-  document.querySelector("body").classList.remove("hidden");
-};
 
+  document.querySelector("body").classList.remove("hidden");
+  const chatBox = document.getElementById("chatBox");
+  chatBox.scrollTo({
+    top: chatBox.scrollHeight,
+    behavior: "smooth",
+  });
+};
