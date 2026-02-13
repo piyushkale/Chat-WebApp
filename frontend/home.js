184fcd077444be0ff30d65f2a6611fdd1fb10e72
Piyush Kale <97799152+piyushkale@users.noreply.github.com>
2026-02-13 11:28:04 +0530

    Frontend - Long polling

--- frontend/home.js
+++ frontend/home.js
@@ -7,7 +7,7 @@ window.onload = async () => {
   }
 
   document.querySelector("body").classList.remove("hidden");
-  await displayMessages();
+  await loadAllMessages();
   const chatBox = document.getElementById("chatBox");
   chatBox.scrollTo({
     top: chatBox.scrollHeight,
@@ -36,7 +36,7 @@ async function formSendMessage(event) {
   }
 }
 
-async function displayMessages() {
+async function loadAllMessages() {
   try {
     const response = await axios.get("/message/allMessages", {
       headers: { Authorization: token },
@@ -66,16 +66,67 @@ async function displayMessages() {
       li.append(spanTime, spanName);
       ul.appendChild(li);
     });
+    displayMessages(userId);
   } catch (error) {
     console.log(error);
   }
 }
 
-setInterval(() => {
-  displayMessages();
-  const chatBox = document.getElementById("chatBox");
-  chatBox.scrollTo({
-    top: chatBox.scrollHeight,
-    behavior: "smooth",
-  });
-}, 3000);
+async function displayMessages(userId) {
+  try {
+    const response = await axios.get("/message/getMessage", {
+      headers: { Authorization: token },
+    });
+
+    const message = response.data.message;
+    // const userId = response.data.userId;
+
+    const ul = document.getElementById("chatBox");
+
+    const date = new Date(message.createdAt);
+
+    const li = document.createElement("li");
+    li.className =
+      "relative text-2xl max-w-[50%] min-w-[20%] p-2 bg-gray-800/80 wrap-anywhere outline-gray-500/50 outline rounded-md";
+
+    if (message.userId === userId) {
+      li.classList.add("self-end");
+    } else {
+      li.classList.add("self-start");
+    }
+
+    li.innerText = message.chat;
+
+    const spanTime = document.createElement("span");
+    spanTime.className = "absolute right-0 -top-6 text-sm opacity-80";
+    spanTime.innerText = date.toLocaleString();
+
+    const spanName = document.createElement("span");
+    spanName.className = "absolute left-0 -top-7 text-lg";
+    spanName.innerText =
+      message.userId === userId ? "You" : `user ${message.userId}`;
+
+    li.append(spanTime, spanName);
+    ul.appendChild(li);
+
+    ul.scrollTo({
+      top: ul.scrollHeight,
+      behavior: "smooth",
+    });
+
+    // Start waiting again AFTER response
+    displayMessages(userId);
+  } catch (error) {
+    console.log(error);
+    setTimeout(() => displayMessages(userId), 2000);
+  }
+}
+
+// setInterval(() => {
+//   displayMessages();
+//   const chatBox = document.getElementById("chatBox");
+//   chatBox.scrollTo({
+//     top: chatBox.scrollHeight,
+//     behavior: "smooth",
+//   });
+// }, 3000);
