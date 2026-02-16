57ca42c7ad87600ca80d5e09ad7e69f3037022bb
Piyush Kale <97799152+piyushkale@users.noreply.github.com>
2026-02-16 15:31:49 +0530

    Socket.IO Personal Messages (Part 2 - Client-side Setup)

--- backend/socket_io/handlers/personalChat.js
+++ backend/socket_io/handlers/personalChat.js
@@ -1,12 +1,27 @@
+// socket.join(`user_${socket.user.userId}`);
+// socket.on("privateMessage", async (data) => {
+//     / data.receiverId
+//     try {
+//       const response = await createMessage(data.chat, socket.user.userId);
+//       io.to(`user_${data.receiverId}`).emit("privateMessage", response);
+//       io.to(`user_${socket.user.userId}`).emit("privateMessage", response);
+//     } catch (error) {
+//       console.log("Socket error", error);
+//     }
+//   });
+
 const createMessage = require("../../services/createMessage");
 module.exports = (io, socket) => {
-  socket.join(`user_${socket.user.userId}`);
+  socket.on("join-room", (email) => {
+    socket.join(email);
+    console.log("Room joined")
+  });
   socket.on("privateMessage", async (data) => {
     // data.receiverId
     try {
       const response = await createMessage(data.chat, socket.user.userId);
-      io.to(`user_${data.receiverId}`).emit("privateMessage", response);
-      io.to(`user_${socket.user.userId}`).emit("privateMessage", response);
+      io.to(data.currentRoom).emit("privateMessage", response);
+      // io.to(`user_${socket.user.userId}`).emit("privateMessage", response);
     } catch (error) {
       console.log("Socket error", error);
     }
--- backend/socket_io/index.js
+++ backend/socket_io/index.js
@@ -1,6 +1,7 @@
 const { Server } = require("socket.io");
 const authenticate = require("./authMiddleware");
 const chatHandler = require("./handlers/chat");
+const personalChatHandler = require('./handlers/personalChat')
 module.exports = (server) => {
   const io = new Server(server);
   authenticate(io);
@@ -9,5 +10,6 @@ module.exports = (server) => {
     console.log("User connected:", socket.id);
 
     chatHandler(io, socket);
+    personalChatHandler(io,socket);
   });
 };
